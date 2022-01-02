2143e783a4f2e8ea1cf0d5642b8d479131acfcea
lukemunsch <lpmunsch@gmail.com>
2022-01-02 19:49:46 +0000

    add content to py file

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -1,25 +1,23 @@
 import psycopg2
 
 
-# connects to "chinook" database
+# connect to "chinook" database
 connection = psycopg2.connect(database="chinook")
-
 
 # build a cursor object of the database
 cursor = connection.cursor()
 
+# Query 1 - select all records from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist"')
 
-# query1 - select all records from artist table
-cursor.execute('SELECT * FROM "Artist"')
-
-# produce results for the file
+# fetch the results (multiple)
 results = cursor.fetchall()
+
+# fetch the result (single)
 # results = cursor.fetchone()
 
-
-# remember to close connection once finished
+# close the connection
 connection.close()
-
 
 # print results
 for result in results:
